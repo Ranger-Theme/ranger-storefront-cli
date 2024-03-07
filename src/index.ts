c40e7f291fda2d60bd4543bcf25c9ce383a326b5
caesar <caesar.luo@silksoftware.com>
2024-03-07 09:16:20 +0800

    chore: email valie

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-
 import fs from 'node:fs'
 import path from 'node:path'
 import chalk from 'chalk'
 import figlet from 'figlet'
 import inquirer from 'inquirer'
 import { program } from 'commander'
+import type { QuestionCollection } from 'inquirer'
 
 type PkgType = {
   name: string
@@ -58,45 +58,155 @@ program.on('--help', () => {
 
 program.parse(process.argv)
 
-const question = [
-  {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?',
-    validate: (input: string) => {
-      if (input.length === 0) {
-        return 'Please enter your name'
+const runTask =  async () => {
+  const question: QuestionCollection[] = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+      validate: (value: string) => {
+        if (value.length === 0) {
+          return 'Please enter your name'
+        }
+        return true
       }
-      return true
-    }
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email?'
-  },
-  {
-    name: 'vue',
-    type: 'checkbox',
-    message: 'Check the features needed for your project:',
-    choices: [
-      {
-        name: 'Babel',
-        checked: true
-      },
-      {
-        name: 'TypeScript'
-      },
-      {
-        name: 'Progressive Web App (PWA) Support'
-      },
-      {
-        name: 'Router'
-      }
-    ]
-  }
-]
+    },
+    {
+      name: 'email',
+      type: 'input',
+      message: 'What is your email?',
+      validate: (value: string) => {
+        const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{l}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/
+        const pass: boolean = regex.test(value)
 
-inquirer.prompt(question).then((data) => {
-  console.log(data)
-})
+        if (!pass) {
+          return 'Please enter a valid email address.'
+        }
+        return true
+      }
+    },
+    {
+      name: 'description',
+      type: 'input',
+      message: 'What is your description?'
+    },
+    {
+      name: 'type',
+      type: 'list',
+      message: 'Please choose the type of your project?',
+      choices: [
+        {
+          name: 'Using App Router',
+          value: 'app',
+          short: 'Features available in /app',
+        },
+        {
+          name: 'Using Pages Router',
+          value: 'pages',
+          short: 'Features available in /pages'
+        }
+      ]
+    },
+    {
+      name: 'ui',
+      type: 'list',
+      message: 'Please choose the UI framework of your project?',
+      choices: [
+        {
+          name: 'Using Material UI',
+          value: '@mui/material',
+          short: 'Material UI is an open-source React component library that implements Google\'s Material Design.'
+        },
+        {
+          name: 'Using Ant Design',
+          value: 'antd',
+          short: 'Following the Ant Design specification, we developed a React UI library antd that contains a set of high quality components and demos for building rich, interactive user interfaces.'
+        }
+      ]
+    },
+    {
+      name: 'styled',
+      type: 'list',
+      message: 'Please choose the CSS-in-JS library of your project?',
+      choices: [
+        {
+          name: 'Using Emotion',
+          value: '@emotion/react',
+          short: 'Emotion is a performant and flexible CSS-in-JS library, it allows you to style apps quickly with string or object styles.'
+        },
+        {
+          name: 'Using Styled Components',
+          value: 'styled-components',
+          short: 'Visual primitives for the component age. Use the best bits of ES6 and CSS to style your apps without stress'
+        }
+      ]
+    },
+    {
+      name: 'features',
+      type: 'checkbox',
+      message: 'Check the features needed for your project:',
+      choices: [
+        {
+          name: 'TypeScript',
+          value: 'typescript',
+          checked: true,
+        },
+        {
+          name: 'Nextjs Middleware',
+          value: 'middeware'
+        },
+        {
+          name: 'Nextjs Api Http Proxy',
+          value: 'proxy'
+        },
+        {
+          name: 'Sentry Recording Support',
+          value: 'sentry'
+        },
+        {
+          name: 'Progressive Web App (PWA) Support',
+          value: 'pwa'
+        }
+      ]
+    },
+    {
+      name: 'package',
+      type: 'list',
+      message: 'Select a package manager',
+      choices: [
+        {
+          name: 'npm',
+          value: 'npm',
+          short: 'npm is the most popular package manager',
+        },
+        {
+          name: 'yarn',
+          value: 'yarn',
+          short: 'yarn is an awesome package manager',
+        },
+        {
+          name: 'cnpm',
+          value: 'cnpm',
+          short: 'cnpm is quick install speed manager',
+        },
+        {
+          name: 'pnpm',
+          value: 'pnpm',
+          short: 'pnpm is most used package manager',
+        },
+      ],
+    },
+    {
+      name: 'install',
+      type: 'confirm',
+      message: 'Do you want to install dependencies now?',
+    }
+  ]
+  
+  await inquirer.prompt(question).then((data) => {
+    console.log(data)
+  })
+}
+
+
+runTask()
